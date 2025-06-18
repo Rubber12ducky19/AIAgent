1a02fae4a90b35a61df07d5c684838d3260ffe9a
Rubber12Ducky19 <stoltzjason@yahoo.com>
2025-06-18 14:34:43 -0700

    B:CH1,L5: added exit code, and command line arguement.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-#When the program starts, load the environment variables from .env using dotenv and read the API key
-
-from google import genai
-#print(f"API Key: {api_key}") #Test to see if api-key exists.
 client = genai.Client(api_key=api_key)
-#Use the API to create a new instance of the Gemini Client
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-      contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-#generate_content() returns a GenerateContentResponse object.
-print(response.text)
+if len(sys.argv) > 1:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1])
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(f"Error: No prompt provided.")
+    sys.exit(1)
