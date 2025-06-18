4fea22ab96dd679e10ee9b86b1c789cb71de7819
Rubber12Ducky19 <stoltzjason@yahoo.com>
2025-06-18 15:10:51 -0700

    C:CH1,L5 Finished : added --verbose arguement

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-if len(sys.argv) > 1:
+user_prompt = sys.argv
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt[1])]),]
+#print (f"User prompt: {user_prompt[1]}")
+if len(user_prompt) > 1:
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1])
+        contents=messages,)
 
     print(response.text)
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in user_prompt:
+        print(f"User prompt: {user_prompt[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 else:
     print(f"Error: No prompt provided.")
     sys.exit(1)
